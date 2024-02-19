cdac892956c80a23e960dfed4f3820b3c0078e98
sanjay <arjunsanjay0@gmail.com>
2024-02-19 10:22:12 +0530

    date string error fix

--- src/app/_components/date-formatter.tsx
+++ src/app/_components/date-formatter.tsx
@@ -1,12 +1,12 @@
-import { parseISO, format } from "date-fns";
+import { toDate, format } from "date-fns";
 
 type Props = {
   dateString: string;
 };
 
 const DateFormatter = ({ dateString }: Props) => {
-  const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, "LLLL	d, yyyy")}</time>;
+  const date = toDate(dateString); // Convert the date string to a Date object
+  return <time dateTime={dateString}>{format(date, "LLLL d, yyyy")}</time>; // Format the date
 };
 
 export default DateFormatter;
